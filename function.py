c8ea5f51edd25dbea91c32062c8551c3797526a4
Corentin Banier <corentin.banier@etu.univ-nantes.fr>
2022-01-10 17:32:12 +0100

    add khi2 test

--- function.py
+++ function.py
@@ -56,6 +56,11 @@ def variance(data):
     return np.var(data)
 
 
+# Calcule l'écart_type
+def ecart_type(var):
+    return sqrt(var)
+
+
 # Vérifie si le dossier output/ existe, le créer sinon
 def check_output_dir():
     if not os.path.exists('output/'):
@@ -68,7 +73,7 @@ def normal_approximation(data):
     fig, ax = plt.subplots()
 
     # affiche l'histogramme des données
-    bins: List[float] = [i for i in range(int(data[-1]) - 1, int(data[0]) + 2)]
+    bins: List[float] = [i for i in range(int(data[-1]), int(data[0]) + 1)]
     ax.hist(data, bins=bins, density=True, alpha=0.6, color='green', edgecolor='k', label='histogramme')
     check_output_dir()
     fig.savefig('output/histogramme.png')
@@ -87,6 +92,75 @@ def normal_approximation(data):
 
     check_output_dir()
     fig.savefig('output/approximation.png')
+    return x, p
+
+
+# Détermine l'effectif observé ce trouvant sur l'intervalle : [interval; interval + 1[
+def effectif_obs(x, interval):
+    eff: List = []
+    cpt: int = 0
+    i = 0
+    while i < len(x):
+        if interval <= x[i] < interval + 1:
+            cpt += 1
+        else:
+            eff.append(cpt)
+            interval += 1
+            cpt = 1
+        i += 1
+    eff.append(cpt)
+    return eff
+
+
+# Détermine la fréquence de chaque valeur de x dans l'intervalle : [interval; interval +1[
+def freq_theoric(x, p, interval):
+    freq: List = []
+    cpt: int = 0
+    i = 0
+    while i < len(x):
+        if interval <= x[i] < interval + 1:
+            cpt += p[i]
+        else:
+            freq.append(cpt)
+            interval += 1
+            cpt = p[i]
+        i += 1
+    freq.append(cpt)
+    return freq
+
+
+# Détermine la fréquence théorique de la loi normale
+def effectif_the(x, freq, interval):
+    for i in range(len(x)):
+        x[i] *= freq[i]
+    return x
+
+
+# Calcule la mesure du khi2
+def mesure_khi2(effectif_observe, effectif_theorique):
+    res: int = 0
+    for i in range(len(effectif_theorique)):
+        res += ((effectif_observe[i] - effectif_theorique[i]) ** 2) / effectif_theorique[i]
+    return res
+
+
+# Détermine k pour la loi du khi2
+def k_khi2(loi, nb_cases):
+    if loi == 'normale':
+        return nb_cases - 3
+    else:
+        pass
+
+
+# Effectue le test du khi2
+def test_khi2(k, d_root, error):
+    val = chi2.ppf(1-error, k)
+    if d_root > val:
+        print("H0 est rejeté")
+        print("X ne suit pas une loi normale")
+    else:
+        print("H0 est accepté")
+        print("X suit une loi normale")
 
 
 # Dessine l'histogramme correspond aux valeurs de data
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from function import *
 
+
+# la méthode .reverse() ne marche pas sur la liste 'numbers'
+def reverse(L):
+    rev = []
+    for elem in reversed(L):
+        rev.append(elem)
+    return rev
+
+
 if __name__ == "__main__":
     data = convert_all_data_to_list('moyenne10km.csv')
     numbers = get_data(data)
@@ -21,8 +30,32 @@ if __name__ == "__main__":
     # affiche la variance
     print('Variance : ', round(variance(numbers), 4))
 
-    # affiche l'histogramme de la série avec l'approximation de la loi normale
-    normal_approximation(numbers)
+    # affiche l'écrart type
+    print('Ecart-type : ', round(ecart_type(variance(numbers)), 4))
 
-    # affiche l'histogramme de la série avec l'approximation de la loi du khi2
-    # khi2_approximation(numbers)
+    # affiche l'histogramme de la série avec l'approximation de la loi normale
+    # ou enregistre les histogrammes dans le dossier output/
+    x, p = normal_approximation(numbers)
+
+    numbers_rev = reverse(numbers)
+    # affiche l'effectif observé de la série sur les intervalle de [i; i+1[
+    observed = effectif_obs(numbers_rev, 6)
+    print('eff observé = ', observed)
+
+    # affiche l'effectif théorique pour chaque intervalle de [i; i+1[
+    theoric = effectif_the(effectif_obs(x, 6), freq_theoric(x, p, 6), 6)
+    print('eff théorique = ', theoric)
+
+    # affiche la mesure du khi2
+    d_root = mesure_khi2(observed, theoric)
+    print('Mesure du khi2, d^2 = ', round(d_root, 3))
+
+    # affiche le paramètre k pour la loi du khi2
+    k = k_khi2('normale', len(theoric))
+    print('k = ', k)
+
+    # Résultat du test
+    print("\n/-----------------------")
+    print("Résultat: ")
+    test_khi2(k, d_root, 0.05)
+    print("-----------------------/")
